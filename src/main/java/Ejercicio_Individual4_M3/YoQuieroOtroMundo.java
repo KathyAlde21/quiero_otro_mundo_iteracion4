053a47265a9d23d35161f0f764ebb3e29fa2b9c9
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-11-05 12:01:53 -0300

    primer commit

---
+++ src/main/java/Ejercicio_Individual4_M3/YoQuieroOtroMundo.java
@@ -0,0 +1,14 @@
+package Ejercicio_Individual4_M3;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kathy
+ */
+public class YoQuieroOtroMundo {
+    
+}
